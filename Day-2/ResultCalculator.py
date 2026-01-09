c06577f7fe7c3973a06b220beace9f68b6e0895a
Arman Hossain <hossain4arman@gmail.com>
2026-01-09 16:54:39 +0600

    Day - 2 Setup-Grading Manager

---
+++ Day-2/ResultCalculator.py
@@ -0,0 +1,18 @@
+
+def get_number(value):
+     while True:
+          try:
+               return input(float(value))
+          except ValueError:
+               print("Invalid value Format! Try again")
+
+
+
+def main():
+    print("#######----Check Your Result Grading---#######")
+    
+
+
+
+if __name__ == "__main__":
+     main()
